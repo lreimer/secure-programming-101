93211c6f410099a0afb4a1f5f16b74b893173981
KernelKaput <kernelkaput@gmail.com>
2015-10-23 11:19:36 +0200

    Added secure web application skeleton.

---
+++ secpro-webapp/src/main/java/de/qaware/campus/secpro/web/SecureRestAPI.java
@@ -0,0 +1,14 @@
+package de.qaware.campus.secpro.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Exposes our secure REST API resources.
+ *
+ * @author mario-leander.reimer
+ */
+@ApplicationPath("api")
+public class SecureRestAPI extends Application {
+
+}
